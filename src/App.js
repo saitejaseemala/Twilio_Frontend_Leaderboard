5ac00e9ecdb5d419f023a8feab1f06ebb599550d
saitejaseemala <saitejaseemala.2424@gmail.com>
2022-05-10 14:01:25 +0530

    Leaderboard solution

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import './App.css';
-import LeaderBoard from './Component/LeaderBoard';
-import 'h8k-components';
+import React from "react";
+import "./App.css";
+import LeaderBoard from "./Component/LeaderBoard";
+import "h8k-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const title = "SPA - LeaderBoard";
 
 function App() {
-	return (
-		<div className="App">
-			<h8k-navbar header={title}></h8k-navbar>
-			<LeaderBoard />
-		</div>
-	);
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <LeaderBoard sortItem="rank" />
+          </Route>
+          <Route path="/rank" exact>
+            <LeaderBoard sortItem="rank" />
+          </Route>
+          <Route path="/points" exact>
+            <LeaderBoard sortItem="points" />
+          </Route>
+          <Route path="/name" exact>
+            <LeaderBoard sortItem="name" />
+          </Route>
+          <Route path="/age" exact>
+            <LeaderBoard sortItem="age" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Component/LeaderBoard.js
+++ src/Component/LeaderBoard.js
@@ -1,42 +1,112 @@
-import React from 'react';
-import { response } from '../response';
+import React, { useState } from "react";
+import { response } from "../response";
+import { Link } from "react-router-dom";
 
-const index = 0;
+function LeaderBoard({ sortItem }) {
+  const responseList = response.list;
+  const [list, setList] = useState(() => {
+    let sortedList;
+    sortedList =
+      sortItem === "name"
+        ? responseList.sort((a, b) => (a.name > b.name ? 1 : -1))
+        : responseList.sort((a, b) => a[sortItem] - b[sortItem]);
+    return sortedList;
+  });
 
-function LeaderBoard(props) {
-	const { rank, name, points, age} = response.list[index];
-	return (
-		<div className="text-center mt-50">
-			<div>
-				<div>
-					<button data-testid="route-rank" className='outlined' type="button">Rank</button>
-					<button data-testid="route-name" className='outlined' type="button">Name</button>
-					<button data-testid="route-points" className='outlined' type="button">Points</button>
-					<button data-testid="route-age" className='outlined' type="button">Age</button>
-				</div>
-			</div>
-			<div className="card mx-auto pb-20 mb-30" style={{ width: '50%' }}>
-				<table className="mt-50" data-testid="app-table">
-					<thead>
-						<tr>
-							<th>Rank</th>
-							<th>Name</th>
-							<th className="numeric">Points</th>
-							<th className="numeric">Age</th>
-						</tr>
-					</thead>
-					<tbody data-testid="app-tbody">
-						<tr key={rank}>
-							<td data-testid={`rank-${index}`}>{rank}</td>
-							<td data-testid={`name-${index}`}>{name}</td>
-							<td data-testid={`points-${index}`} className="numeric">{points}</td>
-							<td data-testid={`age-${index}`} className="numeric">{age}</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  const sortHandler = (e, sort) => {
+    let sortedList;
+
+    sortedList =
+      sort === "name"
+        ? responseList.sort((a, b) => (a.name > b.name ? 1 : -1))
+        : responseList.sort((a, b) => a[sort] - b[sort]);
+    setList(sortedList);
+  };
+
+  return (
+    <div className="text-center mt-50">
+      <div>
+        <div>
+          <Link to="/rank">
+            <button
+              data-testid="route-rank"
+              className={`outlined`}
+              type="button"
+              onClick={(e) => {
+                sortHandler(e, "rank");
+              }}
+            >
+              Rank
+            </button>
+          </Link>
+          <Link to="/name">
+            <button
+              data-testid="route-name"
+              className="outlined"
+              type="button"
+              onClick={(e) => {
+                sortHandler(e, "name");
+              }}
+            >
+              Name
+            </button>
+          </Link>
+          <Link to="/points">
+            <button
+              data-testid="route-points"
+              className="outlined"
+              type="button"
+              onClick={(e) => {
+                sortHandler(e, "points");
+              }}
+            >
+              Points
+            </button>
+          </Link>
+          <Link to="/age">
+            <button
+              data-testid="route-age"
+              className="outlined"
+              type="button"
+              onClick={(e) => {
+                sortHandler(e, "age");
+              }}
+            >
+              Age
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="card mx-auto pb-20 mb-30" style={{ width: "50%" }}>
+        <table className="mt-50" data-testid="app-table">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th className="numeric">Points</th>
+              <th className="numeric">Age</th>
+            </tr>
+          </thead>
+          <tbody data-testid="app-tbody">
+            {list.map((item, index) => {
+              return (
+                <tr key={item.rank}>
+                  <td data-testid={`rank-${index}`}>{item.rank}</td>
+                  <td data-testid={`name-${index}`}>{item.name}</td>
+                  <td data-testid={`points-${index}`} className="numeric">
+                    {item.points}
+                  </td>
+                  <td data-testid={`age-${index}`} className="numeric">
+                    {item.age}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default LeaderBoard;
